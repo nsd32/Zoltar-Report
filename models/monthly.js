528bab6470c1ca2174c8d0b74ef350081b850bbe
Rob Warren <robw219@gmail.com>
2017-10-31 13:02:56 -0600

    Added Traffic and Monthly Usage Seq models

---
+++ models/monthly.js
@@ -0,0 +1,101 @@
+module.exports = function(sequelize, DataTypes) {
+	var monthUsage = sequelize.define('monthUsage', {
+        sessions: {
+			type: DataTypes.INTEGER,
+			allowNull: false,
+			validate: {
+				isNumeric: true
+			}
+        },
+        pageviews: {
+			type: DataTypes.INTEGER,
+			allowNull: false,
+			validate: {
+				isNumeric: true
+			}
+        },
+        users: {
+			type: DataTypes.INTEGER,
+			allowNull: false,
+			validate: {
+				isNumeric: true
+			}
+        },
+        pageviewsBySession: {
+			type: DataTypes.DECIMAL(2,2),
+			allowNull: false,
+			validate: {
+				isDecimal: true
+			}
+        },
+        exitRate: {
+			type: DataTypes.DECIMAL(2,2),
+			allowNull: false,
+			validate: {
+				isDecimal: true
+			}
+        },
+        bounceRate: {
+			type: DataTypes.DECIMAL(2,2),
+			allowNull: false,
+			validate: {
+				isDecimal: true
+			}
+        },
+        newSession: {
+			type: DataTypes.DECIMAL(2,2),
+			allowNull: false,
+			validate: {
+				isDecimal: true
+			}
+        },
+        avgSession: {
+			type: DataTypes.TIME,
+			allowNull: false,
+			validate: {
+	
+			}
+        },
+        goalCompletion: {
+			type: DataTypes.INTEGER,
+			allowNull: false,
+			validate: {
+				isNumeric: true
+			}
+		},
+		company_id: {
+			type: DataTypes.INTEGER,
+			allowNull: false,
+			validate: {
+				isNumeric: true
+			}
+        },
+        property_id: {
+			type: DataTypes.INTEGER,
+			allowNull: false,
+			validate: {
+				isNumeric: true
+			}
+        },
+        view_id: {
+			type: DataTypes.INTEGER,
+			allowNull: false,
+			validate: {
+				isNumeric: true
+			}
+        },
+        start_date: {
+			type: DataTypes.DATE,
+			allowNull: false,
+			validate: {
+				
+			}
+        },
+        end_date: {
+			type: DataTypes.DATE,
+			allowNull: false,
+			validate: {
+				
+			}
+        }
+    });
