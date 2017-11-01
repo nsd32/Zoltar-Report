63baaa08556eac2caaadbd3248ab296aac7e325c
Erik Streff <Estreff@gmail.com>
2017-10-31 20:40:59 -0600

    Create Company modal design

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,7 +1,15 @@
 // do we need two route files? route below is not an api route
 
+
+
 module.exports = function(app) {
-	app.get('/', function(req, res) {
+	app.get('/dashboard', function(req, res) {
+		res.render('dashboard');
+	})
+};
+
+module.exports = function(app) {
+	app.get('/*', function(req, res) {
 		res.render('index');
 	})
 };
--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.text());
 app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 
-var db = require('./models');
+var db = require('./models'); 
 
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
@@ -19,7 +19,7 @@ app.use(express.static("public"));
 
 require("./routes/api-routes.js")(app);
 
-db.sequelize.sync({ force: true }).then(function() {
+db.sequelize.sync(/*{ force: true }*/).then(function() {
 	app.listen(PORT, function() {
 		console.log('App listening on PORT ' + PORT);
 	});
