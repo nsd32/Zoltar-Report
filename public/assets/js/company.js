93ab49288aaa702db32f6e7fb139c435680259ef
Erik Streff <Estreff@gmail.com>
2017-11-01 18:03:58 -0600

    api-routes update

--- public/assets/js/company.js
+++ public/assets/js/company.js
@@ -3,4 +3,14 @@ var company = {
     account_number: account-input.val().trim()
 };
 
+var property = {
+    property_name: propertyName-input.val().trim(),
+    tracking_id: trackingID-input.val().trim()
+};
+
+var view = {
+    view_name: viewName-input.val().trim(),
+    ga_view_id: viewID-input.val().trim()
+};
+
 
--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,15 +1,23 @@
 // do we need two route files? route below is not an api route
 
-
-
 module.exports = function(app) {
+	app.get('/', function(req, res) {
+		res.render('index');
+	});
+
 	app.get('/dashboard', function(req, res) {
 		res.render('dashboard');
-	})
-};
+	});
 
-module.exports = function(app) {
+	app.get('/createCompany/new', function(req, res) {
+		db.Author.findAll({
+			include: [db.Post]
+		}).then(function(dbAuthor) {
+			res.json(dbAuthor);
+		});
+	});
+	
 	app.get('/*', function(req, res) {
 		res.render('index');
-	})
+	});
 };
