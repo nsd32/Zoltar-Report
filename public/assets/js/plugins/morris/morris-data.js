06e5efde0924ebd3bee1be06173ae8457f1ba488
Lea Abeyta <lea.abeyta@gmail.com>
2017-11-02 13:39:07 -0600

    populating pie chart

--- public/assets/js/plugins/morris/morris-data.js
+++ public/assets/js/plugins/morris/morris-data.js
@@ -81,67 +81,55 @@
     var avgSessionDuration = response.result.reports[0].data.rows[0].metrics[0].values[7];
     var goalCompletions = response.result.reports[0].data.rows[0].metrics[0].values[8];
     console.log(sessions)
-     // Area Chart
-    Morris.Area({
-        element: 'morris-area-chart',
+
+     // Donut Chart
+    Morris.Donut({
+        element: 'morris-donut-chart',
         data: [{
-            period: '2010 Q1',
-            sessions: sessions,
-            ipad: null,
-            itouch: 2647
+            label: "Sessions",
+            value: sessions
         }, {
-            period: '2010 Q2',
-            iphone: 2778,
-            ipad: 2294,
-            itouch: 2441
+            label: "Pageviews",
+            value: pageviews
         }, {
-            period: '2010 Q3',
-            iphone: 4912,
-            ipad: 1969,
-            itouch: 2501
-        }, {
-            period: '2010 Q4',
-            iphone: 3767,
-            ipad: 3597,
-            itouch: 5689
-        }, {
-            period: '2011 Q1',
-            iphone: 6810,
-            ipad: 1914,
-            itouch: 2293
-        }, {
-            period: '2011 Q2',
-            iphone: 5670,
-            ipad: 4293,
-            itouch: 1881
-        }, {
-            period: '2011 Q3',
-            iphone: 4820,
-            ipad: 3795,
-            itouch: 1588
-        }, {
-            period: '2011 Q4',
-            iphone: 15073,
-            ipad: 5967,
-            itouch: 5175
-        }, {
-            period: '2012 Q1',
-            iphone: 10687,
-            ipad: 4460,
-            itouch: 2028
-        }, {
-            period: '2012 Q2',
-            iphone: 8432,
-            ipad: 5713,
-            itouch: 1791
+            label: "Users",
+            value: users
         }],
-        xkey: 'period',
-        ykeys: ['iphone', 'ipad', 'itouch'],
-        labels: ['iPhone', 'iPad', 'iPod Touch'],
-        pointSize: 2,
-        hideHover: 'auto',
         resize: true
     });
+    
+     // Area Chart
+    // Morris.Area({
+    //     element: 'morris-area-chart',
+    //     data: [{
+    //         period: 'Sunday',
+    //         pageviews: 100
+    //     }, {
+    //         period: 'Monday',
+    //         pageviews: 120
+    //     }, {
+    //         period: 'Tuesday',
+    //         pageviews: 90
+    //     }, {
+    //         period: 'Wednesday',
+    //         pageviews: 75
+    //     }, {
+    //         period: 'Thursday',
+    //         pageviews: 130
+    //     }, {
+    //         period: 'Friday',
+    //         pageviews: 50
+    //     }, {
+    //         period: 'Saturday',
+    //         pageviews: 175
+    //     }],
+    //     xkey: 'period',
+    //     ykeys: ['pageviews'],
+    //     labels: ['pageviews'],
+    //     pointSize: 2,
+    //     hideHover: 'auto',
+    //     resize: true
+    // });
 
     // var newTest = {
     //   sessions: sessions,
@@ -166,21 +154,7 @@
 
    
 
-    // // Donut Chart
-    // Morris.Donut({
-    //     element: 'morris-donut-chart',
-    //     data: [{
-    //         label: "Download Sales",
-    //         value: 12
-    //     }, {
-    //         label: "In-Store Sales",
-    //         value: 30
-    //     }, {
-    //         label: "Mail-Order Sales",
-    //         value: 20
-    //     }],
-    //     resize: true
-    // });
+   
 
     // // Line Chart
     // Morris.Line({
